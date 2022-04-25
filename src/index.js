092e2a3622ddadd632e6422301c141ab10c828c3
Kurilyna Darya <d.fotina@mail.ru>
2022-04-25 20:54:15 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './scss/about.scss';
+import './scss/style.scss';
+import './scss/work.scss';
+import './scss/workers.scss';
+import './scss/reset.scss';
+import './scss/download.scss';
+import './scss/footer.scss';
+import './scss/header.scss';
